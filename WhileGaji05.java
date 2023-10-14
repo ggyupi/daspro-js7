5de83132844d66474cb52f42b41ed0934c3ce894
ggyupi <beryl32451@gmail.com>
2023-10-15 00:38:10 +0700

    gaji

---
+++ WhileGaji05.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+/**
+ * WhileGaji05
+ */
+public class WhileGaji05 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jmlKaryawan, jmlJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan : ");
+        jmlKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jmlKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i + 1) + " : ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur : ");
+            jmlJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jmlJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jmlJamLembur * 75000;
+            }
+            
+            totalGajiLembur += gajiLembur; 
+        }
+        System.out.print("Total gaji lembur : " + totalGajiLembur);
+        scan.close();
+    }
+}
