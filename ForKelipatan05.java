7f84891dd5a594ac0fc74d18581d1872c21748c7
ggyupi <beryl32451@gmail.com>
2023-10-14 17:40:41 +0700

    modifikasi

--- ForKelipatan05.java
+++ ForKelipatan05.java
@@ -7,6 +7,7 @@ public class ForKelipatan05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0;
+        float rerataKelipatan = 0f;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
@@ -15,10 +16,13 @@ public class ForKelipatan05 {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+                rerataKelipatan++;
             }
         }
+            rerataKelipatan = jumlah / rerataKelipatan;
             System.out.printf("Banyaknya bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
             System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            System.out.printf("Rata-rata kelipatan %d dari 1 sampai 50 adalah %f\n", kelipatan, rerataKelipatan);
             scan.close();
     }
 }
